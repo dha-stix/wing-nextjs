52d1095cfc4065ffc0049b80cdd509b7864acf53
David Asaolu <asaoludavid234@yahoo.com>
2024-05-10 06:18:44 +0100

    First commit

---
+++ src/backend/main.ts
@@ -0,0 +1,53 @@
+import { main, cloud, inflight, lift } from "@wingcloud/framework";
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import React from "@winglibs/react";
+
+main((root, test) => {
+	let api = new cloud.Api(root, "api", { cors: true });
+
+	api.post(
+		"/api",
+		inflight(async (ctx, request) => {
+			const data = JSON.parse(request.body!);
+			const apiKey = "sk-proj-YrEeQW9fE7K6DapCZkJaT3BlbkFJ96Yyz32JbM16XuQGDAHV";
+
+			const chatModel = new ChatOpenAI({ apiKey });
+
+			const prompt = ChatPromptTemplate.fromMessages([
+				[
+					"system",
+					"You are a webscraper that retrieves information about websites. Ask me anything about a website and I will try to answer it.",
+				],
+				["user", "{input}"],
+			]);
+			const chain = prompt.pipe(chatModel);
+
+			const response = await chain.invoke({
+				input: `${data.question} using this website information: ${data.websiteData}`,
+			});
+
+			if (response?.content) {
+				return {
+					status: 200,
+					body: response.content,
+				};
+			}
+
+			return undefined;
+		})
+	);
+
+	api.get(
+		"/test",
+		inflight(async () => {
+			return {
+				status: 200,
+				body: "Hello world",
+			};
+		})
+	);
+
+	const react = new React.App(root, "react", { projectPath: "../frontend" });
+	react.addEnvironment("api_url", api.url);
+});

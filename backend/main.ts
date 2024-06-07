228a4698459117d208f4083eeb9759ece21ed651
David Asaolu <asaoludavid234@yahoo.com>
2024-06-07 12:18:52 +0100

    fix(): Use Langchain retrievals

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,11 @@
 import { main, cloud, inflight, lift } from "@wingcloud/framework";
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { createRetrievalChain } from "langchain/chains/retrieval";
 import React from "@winglibs/react";
 
 main((root, test) => {
@@ -16,42 +21,66 @@ main((root, test) => {
 			.inflight(async (ctx, request) => {
 				const apiKey = await ctx.secret.value();
 
-				const data = JSON.parse(request.body!)
+				const {question, pageURL} = JSON.parse(request.body!);
 
-				const chatModel = new ChatOpenAI({ apiKey });
-
-				const prompt = ChatPromptTemplate.fromMessages([
-					[
-						"system",
-						"You are a webscraper that retrieves information about websites. Ask me anything about a website and I will try to answer it.",
-					],
-					["user", "{input}"],
-				]);
-				const chain = prompt.pipe(chatModel);
-
-				const response = await chain.invoke({
-					input: `${data.question} using this website information: ${data.websiteData}`,
+				const chatModel = new ChatOpenAI({
+					apiKey,
+					model: "gpt-3.5-turbo-1106",
 				});
 
-				if (typeof response.content === "string") {
+				const splitter = new RecursiveCharacterTextSplitter({
+					chunkSize: 200,
+					chunkOverlap: 20,
+				});
+
+				const loader = new CheerioWebBaseLoader(pageURL);
+				const docs = await loader.load();
+
+				const splitDocs = await splitter.splitDocuments(docs);
+
+				const embeddings = new OpenAIEmbeddings({
+					apiKey,
+				});
+				const vectorStore = await MemoryVectorStore.fromDocuments(
+					splitDocs,
+					embeddings
+				);
+
+				const retriever = vectorStore.asRetriever({
+					k: 1,
+				});
+
+				const prompt = ChatPromptTemplate.fromTemplate(`
+				Answer this question.
+				Context: {context}
+				Question: {input}	
+				`);
+
+				const chain = await createStuffDocumentsChain({
+					//@ts-ignore
+					llm: chatModel,
+					//@ts-ignore
+					prompt: prompt,
+				});
+
+				const retrievalChain = await createRetrievalChain({
+					combineDocsChain: chain,
+					retriever,
+				});
+
+				const response = await retrievalChain.invoke({
+					input: `${question}`,
+				});
+
+				if (response) {
 					return {
 						status: 200,
-						body: response.content
+						body: response.answer,
 					};
 				}
 
 				return undefined;
 			})
-	);
-
-	api.get(
-		"/test",
-		inflight(async () => {
-			return {
-				status: 200,
-				body: "Hello world",
-			};
-		})
 	);
 
 	const react = new React.App(root, "react", { projectPath: "../frontend" });
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,129 +1,82 @@
-"use client"
+"use client";
 import { useState } from "react";
 interface WingEnv {
-  api_url: string;
-  // Add other properties if needed
+	api_url: string;
+	// Add other properties if needed
 }
-  declare global {
-    interface Window {
-      wingEnv: WingEnv;
-    }
-  }
+declare global {
+	interface Window {
+		wingEnv: WingEnv;
+	}
+}
 
 export default function Home() {
-  const [sitemapURL, setSitemapURL] = useState<string>("")
-  const [question, setQuestion] = useState<string>("")
-  const [websiteData, setWebsiteData] = useState<string | null>(null)
-  const [disable, setDisable] = useState<boolean>(false)
-  const [response, setResponse] = useState<string | null>(null)
+	const [sitemapURL, setSitemapURL] = useState<string>("");
+	const [question, setQuestion] = useState<string>("");
+	const [disable, setDisable] = useState<boolean>(false);
+	const [response, setResponse] = useState<string | null>(null);
 
-    // Helper function to extract text nodes from an XML document
-  function extractTextContents(xmlDoc: Document): string[] {
-    let texts: string[] = [];
-    const walker = document.createTreeWalker(xmlDoc, NodeFilter.SHOW_TEXT, null);
-    
-    let node: Node | null;
-    while (node = walker.nextNode()) {
-        const text = node.nodeValue?.trim();
-        if (text) {
-            texts.push(text);
-        }
-    }
-    return texts;
-  }
-  
-  const handleFetchSitemap =  async (e: React.FormEvent) => {
-    e.preventDefault()
-    try { 
-      const response = await fetch(sitemapURL);
-      if (!response.ok) {
-        throw new Error(`Sitemap not found Status: ${response.status}`);
-      }
-      const text = await response.text();
-      const xmlDoc = new DOMParser().parseFromString(text, "text/xml");
-      const data = extractTextContents(xmlDoc);
-      const stringData = data.join(" ");
-      setWebsiteData(stringData)
-      setSitemapURL("")
-    } catch (err) {
-      console.error(err);
-    }
-    
-  }
+	const handleUserQuery = async (e: React.FormEvent) => {
+		e.preventDefault();
+		setDisable(true);
+		try {
+			const request = await fetch(`${window.wingEnv.api_url}/api`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ question, pageURL: sitemapURL }),
+			});
+			const response = await request.text();
+			setResponse(response);
+			setDisable(false);
+		} catch (err) {
+			console.error(err);
+			setDisable(false);
+		}
+	};
 
+	return (
+		<main className='w-full md:px-8 px-3 py-8'>
+			<h2 className='font-bold text-2xl mb-8 text-center text-blue-600'>Documentation Bot with Wing & LangChain</h2>
 
-  const handleUserQuery = async (e: React.FormEvent) => { 
-    e.preventDefault()
-    setDisable(true)
-    try {
-      const request = await fetch(`${window.wingEnv.api_url}/api`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question, websiteData }),
-    });
-      const response = await request.text();
-      console.log({ response })
-      setResponse(response)
-      setDisable(false)
-    }catch (err) {
-      console.error(err);
-      setDisable(false)
-    }
-    
-  }
+			<form onSubmit={handleUserQuery} className='mb-8'>
+				<label className='block mb-2 text-sm text-gray-500'>Webpage URL</label>
+				<input
+					type='url'
+					className='w-full mb-4 p-4 rounded-md border text-sm border-gray-300'
+					placeholder='https://www.winglang.io/docs/concepts/why-wing'
+					required
+					value={sitemapURL}
+					onChange={(e) => setSitemapURL(e.target.value)}
+				/>
 
-  return (
-    <main className="w-full px-8 py-8">
-      <h2 className="font-bold text-2xl mb-8">Q&A Web Bot</h2>
-      {!websiteData && (
-         <form onSubmit={handleFetchSitemap} className="mb-8">
-        <label className="block mb-2">Sitemap URL</label>
-        <input
-          type="url"
-          className="w-full mb-4 p-4 rounded-md border border-gray-300"
-          placeholder="http://localhost:3001/api"
-          required
-            value={sitemapURL}
+				<label className='block mb-2 text-sm text-gray-500'>
+					Ask any questions related to the page URL above
+				</label>
+				<textarea
+					rows={5}
+					className='w-full mb-4 p-4 text-sm rounded-md border border-gray-300'
+					placeholder='What is Winglang? OR Why should I use Winglang? OR How does Winglang work?'
+					required
+					value={question}
+					onChange={(e) => setQuestion(e.target.value)}
+				/>
 
-          onChange={e => setSitemapURL(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-8 text-lg py-3 rounded"
-        >
-          Submit
-        </button>
-      </form>
+				<button
+					type='submit'
+					disabled={disable}
+					className='bg-blue-500 text-white px-8 py-3 rounded'
+				>
+					{disable ? "Loading..." : "Ask Question"}
+				</button>
+			</form>
 
-      )}
-     
-      {websiteData && (
-        <form onSubmit={handleUserQuery} className="mb-8">
-        <label className="block mb-2">Ask any question based on the website sitemap</label>
-        <input
-          type="text"
-          className="w-full mb-4 p-4 rounded-md border border-gray-300"
-          placeholder="What is Winglang? OR Why should I use Winglang? OR How does Winglang work?"
-          required
-          value={question}
-          onChange={e => setQuestion(e.target.value)}
-        />
-        <button
-            type="submit"
-            disabled={disable}
-          className="bg-blue-500 text-white px-8 text-lg py-3 rounded"
-        >
-          {disable ? "Loading..." : "Ask Question"}
-        </button>
-      </form>
-      )}
-      <div className="bg-gray-100 w-full p-8 rounded-sm shadow-md">
-        <p className="text-gray-600">{response}</p>
-      </div>
-        
-
-    </main>
-  );
+			{response && (
+				<div className='bg-gray-100 w-full p-8 rounded-sm shadow-md'>
+					<p className='text-gray-600'>{response}</p>
+				</div>
+			)}
+		</main>
+	);
 }
